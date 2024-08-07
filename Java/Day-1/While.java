f6faaf39ae84a4b8d3ed58be45fe76cbdbe6ea21
Krishna595956 <kr4785543@gmail.com>
2024-08-07 13:06:40 +0530

    oops started

--- Java/Day-1/While.java
+++ Java/Day-1/While.java
@@ -1,10 +1,10 @@
-class Even{
-    public static void main(String args[]){
-        int num=0;                       //initialization
-        while(num<10){                  //condition
-            System.out.println(num); 
-            num++;                      //updation                           
-        }
-    }
-}
+// class Even{
+//     public static void main(String args[]){
+//         int num=0;                       //initialization
+//         while(num<10){                  //condition
+//             System.out.println(num); 
+//             num++;                      //updation                           
+//         }
+//     }
+// }
 
